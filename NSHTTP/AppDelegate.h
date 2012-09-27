ac26e614b65c429760c538143cc6a13cce4c2947
Ibokan <ibokan@5-28.local>
2012-09-27 09:52:05 +0800

    Initial Commit

---
+++ NSHTTP/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NSHTTP
+//
+//  Created by Ibokan on 12-9-27.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
